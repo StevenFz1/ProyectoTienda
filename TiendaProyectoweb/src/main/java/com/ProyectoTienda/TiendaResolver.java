3da5eba214e9a1d195569fa0ec8e878501f39cad
RandallMataT <107011398+RandallMataT@users.noreply.github.com>
2022-07-13 10:57:24 -0600

    Add files via upload

---
+++ TiendaProyectoweb/src/main/java/com/ProyectoTienda/TiendaResolver.java
@@ -0,0 +1,31 @@
+package com.ProyectoTienda;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class TiendaResolver {
+
+    @Bean
+    public SpringResourceTemplateResolver templatesResolver_0() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(0);
+        return templateResolver;
+    }
+
+    public SpringResourceTemplateResolver templatesResolver_1() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("templates/Cliente");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(1);
+        return templateResolver;
+    }
+}
